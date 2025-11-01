20894a2bc674909213d266eba78355123ccd8cb2
yashverma03 <vermayash2003@gmail.com>
2025-11-01 18:21:59 +0530

    feat: Revise WorkExperience component to enhance descriptions and improve clarity

--- frontend/src/pages/Home/WorkExperience/WorkExperience.tsx
+++ frontend/src/pages/Home/WorkExperience/WorkExperience.tsx
@@ -13,32 +13,31 @@ const experiences: Experience[] = [
     company: 'Oats Tech',
     date: 'Apr 2025 – Present (6 months)',
     description: `• Integrated Nuvei’s Merchant Initiated Transactions (MIT) for subscription payments, scheduled 3 business days in advance, with a custom failed-transaction retry logic, reducing payment failures by 15%
-• Automated a file synchronization pipeline where dashboard file uploads are stored and synced to an AI-context data source, leading to a 90% improvement in AI response relevance
-• Integrated Stripe’s subscription billing and implemented a tiered access system where users can interact with the AI based on usage limits defined by their subscribed plan, reducing AI overuse incidents by 40%`
+• Designed a gamified points and rewards system to incentivize user referrals, payments, subscriptions and enabling gift cards and rent credit redemptions in future payments, increasing monthly active users (MAUs) by 35%
+• Optimized a background data processing pipeline by 10 times, enabling efficient ingestion, normalization, and consolidation of payment data from Central 1, Telpay, and Nuvei, and delivering accurate Convera payout files for reconciliation
+• Implemented Single Sign-On (SSO) integration allowing TenantPay users to seamlessly access Equifax to view credit scores, reducing the number of steps to access credit data by 40%
+• Automated a file synchronization pipeline to seamlessly ingest and sync dashboard uploads from external sources into an AI-context data repository, resulting in a 90% improvement in AI response relevance and accuracy
+• Integrated Stripe’s subscription billing and implemented a tiered access system where users can interact with the AI based on usage limits defined by their subscribed plan, reducing AI overuse incidents by 40%
+`
   },
   {
     title: 'Junior Software Engineer',
     company: 'Oats Tech',
     date: 'Mar 2024 – Mar 2025 (1 year)',
-    description: `• Integrated Trustmatic’s IDV APIs for remote identity verification and developed webhook listeners for real-time status updates, automating tenant approval process and boosting operational efficiency by 56%
-• Developed a strong security system for tenants and managers, featuring multi-factor authentication (MFA) via SMS and email using secure HTTPS-only cookies, login retry restrictions, API rate limiting, and Google reCAPTCHA — resulting in a 80% reduction in brute-force attacks
+    description: `• Integrated Trustmatic’s IDV APIs for remote identity verification with real-time status updates, automating tenant approval process and boosting operational efficiency by 56%
 • Developed a white-label architecture where each company accesses its own tenant portal with unique branding, improving customer retention by 20% through enhanced personalization
 • Implemented OAuth2-based social login, enabling seamless authentication via Google, Apple, Microsoft, and Facebook to enhance user onboarding experience and reduce login friction by 45%
 • Engineered an alerting mechanism to track potentially fraudulent landlord transactions using KYC insights and client risk assessment reports, reducing manual review efforts by 63%
 • Implemented real-time chat functionality using Socket.io, enabling instant bi-directional communication between car owners and garage owners, improving service coordination by 24%
-• Implemented a garage owner search page with Google Maps integration to visually display garage locations, enhancing user experience and improving search discoverability by 40%
-• Created a 3D mapping solution using Mapbox GL and added geocoding API to enable location-based search functionality, enabling users to explore 8,000+ business locations across Canada
-• Built a multilingual home services booking flow with dynamic, service-specific questionnaires and real-time price calculation based on user inputs, streamlining contractor selection and boosting conversion rates by 12%
-• Implemented debouncing on Google Places API requests to minimize redundant calls, reducing API usage by 70%`
+`
   },
   {
     title: 'Software Engineer Intern',
     company: 'Oats Tech',
     date: 'Sep 2023 – Mar 2024 (6 months)',
-    description: `• Engineered an admin dashboard with a  Content Management System (CMS) to manage website content and assets, reducing manual administrative task time by 52%
+    description: `• Engineered a multilingual admin dashboard with a Content Management System (CMS) to manage website content and assets, reducing manual administrative task time by 52%
 • Built a customizable kitchen layout configurator that generated real-time design previews and pricing based on user preferences, improving visualization clarity and boosting user engagement by 47%
-• Optimized API efficiency using TanStack Query's caching mechanism, cutting down redundant network requests and achieving a 15% reduction in AWS EC2 costs
-• Optimized performance using lazy loading techniques and reducing initial page load time by up to 27%`
+`
   }
 ];
 
